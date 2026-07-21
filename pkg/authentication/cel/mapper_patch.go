everettraven/openshift-kubernetes-apiserver

UPSTREAM: <carry>: patch structured authentication for extensibility
so that we can extend the baseline structured
authentication behavior out-of-band.

Signed-off-by: Bryce Palmer <bpalmer@redhat.com>

pkg/authentication/cel/mapper_patch.go
@@ -1,0 +1,36 @@
+package cel
+
+import (
+	"context"
+
+	"github.com/google/cel-go/common/types/traits"
+)
+
+type ExportedMapper struct {
+	*mapper
+}
+
+func NewExportedMapper(compilationResults []CompilationResult) *ExportedMapper {
+	return &ExportedMapper{
+		mapper: &mapper{
+			compilationResults: compilationResults,
+		},
+	}
+}
+
+func (em *ExportedMapper) Eval(ctx context.Context, input VarNameActivation) ([]EvaluationResult, error) {
+	return em.mapper.eval(ctx, input.varNameActivation)
+}
+
+type VarNameActivation struct {
+	*varNameActivation
+}
+
+func NewVarNameActivation(name string, value traits.Mapper) VarNameActivation {
+	return VarNameActivation{
+		varNameActivation: &varNameActivation{
+			name: name,
+			value: value,
+		},
+	}
+}

openshift/kubernetes-apiserver

pkg/authentication/cel/mapper_patch.go
@@ -1,0 +1,36 @@
+package cel
+
+import (
+	"context"
+
+	"github.com/google/cel-go/common/types/traits"
+)
+
+type Mapper struct {
+	*mapper
+}
+
+func NewMapper(compilationResults []CompilationResult) *Mapper {
+	return &Mapper{
+		mapper: &mapper{
+			compilationResults: compilationResults,
+		},
+	}
+}
+
+func (m *Mapper) Eval(ctx context.Context, input VarNameActivation) ([]EvaluationResult, error) {
+	return m.mapper.eval(ctx, input.varNameActivation)
+}
+
+type VarNameActivation struct {
+	*varNameActivation
+}
+
+func NewVarNameActivation(name string, value traits.Mapper) VarNameActivation {
+	return VarNameActivation{
+		varNameActivation: &varNameActivation{
+			name: name,
+			value: value,
+		},
+	}
+}